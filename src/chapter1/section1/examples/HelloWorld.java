22cbd3adf67279033c430e22e024b44407b4ca51
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-29 15:40:48 +0600

    TenHelloWorld problem solved

---
+++ src/chapter1/section1/examples/HelloWorld.java
@@ -0,0 +1,8 @@
+package chapter1.section1.examples;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
---
+++ src/chapter1/section1/exercises/TenHelloWorld.java
@@ -0,0 +1,16 @@
+package chapter1.section1.exercises;
+
+public class TenHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
