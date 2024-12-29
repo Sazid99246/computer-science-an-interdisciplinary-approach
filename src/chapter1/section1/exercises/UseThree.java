d3d22536260d2cf2d466de9f50efed3a73d9470e
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-29 15:47:15 +0600

    UseThree problem solved

---
+++ src/chapter1/section1/exercises/UseThree.java
@@ -0,0 +1,13 @@
+package chapter1.section1.exercises;
+
+public class UseThree {
+    public static void main(String[] args) {
+        System.out.print("Hi ");
+        System.out.println(args[0]);
+        System.out.println(", ");
+        System.out.println(args[1]);
+        System.out.println("and");
+        System.out.println(args[2]);
+        System.out.println(".");
+    }
+}
