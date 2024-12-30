b731c1099e58c89dd6f22184629c46ab15255b46
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-30 21:45:16 +0600

    ThreeSort problem solved

---
+++ src/chapter1/section2/exerecises/ThreeSort.java
@@ -0,0 +1,17 @@
+package chapter1.section2.exerecises;
+
+public class ThreeSort {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        int min = Math.min(a, Math.min(b, c));
+        int max = Math.max(a, Math.max(b, c));
+        int medium = a + b + c - max - min;
+
+        System.out.print(min + " ");
+        System.out.print(medium + " ");
+        System.out.println(max);
+    }
+}
