fd34b3feec3cd53f371ec4d10bbc0d4f918d71ef
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-31 07:03:45 +0600

    CarLoan problem solved

---
+++ src/chapter1/section2/webExercises/CarLoan.java
@@ -0,0 +1,18 @@
+package chapter1.section2.webExercises;
+
+public class CarLoan {
+    public static void main(String[] args) {
+        double P = Double.parseDouble(args[0]);
+        double Y = Double.parseDouble(args[1]);
+        double R = Double.parseDouble(args[2]);
+
+        double n = 12 * Y;
+        double r = (R / 100) / 12;
+
+        double payment = (P * r) / (1 - Math.pow(1 + r, -n));
+        double interest = payment * n - P;
+
+        System.out.println("Monthly payments = " + payment);
+        System.out.println("Total interest   = " + interest);
+    }
+}
