e8008a5ae3a729d4aeb65a22299532b05a4df200
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-31 23:24:42 +0600

    Sum of Sines problem solved

---
+++ src/chapter1/section2/exerecises/SumOfSines.java
@@ -0,0 +1,10 @@
+package chapter1.section2.exerecises;
+
+public class SumOfSines {
+    public static void main(String[] args) {
+        double degrees = Double.parseDouble(args[0]);
+        double radians = Math.toRadians(degrees);
+        double sum = Math.sin(2 * radians) + Math.sin(3 * radians);
+        System.out.println(sum);
+    }
+}
