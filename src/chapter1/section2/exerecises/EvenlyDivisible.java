c5708c022af747efeb4e23364597fd82d72d0c3f
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-31 22:47:05 +0600

    TriangleTest problem solved

---
+++ src/chapter1/section2/exerecises/EvenlyDivisible.java
@@ -0,0 +1,10 @@
+package chapter1.section2.exerecises;
+
+public class EvenlyDivisible {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println(a % b == 0 || b % a == 0);
+    }
+}
---
+++ src/chapter1/section2/exerecises/TriangleTest.java
@@ -0,0 +1,11 @@
+package chapter1.section2.exerecises;
+
+public class TriangleTest {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        System.out.println((a >= b + c) || (b >= a + c) || (c >= a + b));
+    }
+}
