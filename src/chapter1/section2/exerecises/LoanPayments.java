486176f1b001e61a96bb4bf011d81158ff17c2d3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-01 09:21:12 +0600

    Loan Payment problem solved

---
+++ src/chapter1/section2/exerecises/LoanPayments.java
@@ -0,0 +1,14 @@
+package chapter1.section2.exerecises;
+
+public class LoanPayments {
+    public static void main(String[] args) {
+        int t = Integer.parseInt(args[0]);
+        int p = Integer.parseInt(args[1]);
+        int r = Integer.parseInt(args[2]);
+
+        double totalAmount = p * Math.exp(r * t);
+        System.out.println(totalAmount / (t * 12));
+
+        System.out.println();
+    }
+}
