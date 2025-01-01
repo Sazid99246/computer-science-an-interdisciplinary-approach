41e98d5dabb12578cb5ffe6f3b26224267d06a0d
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-01 14:01:10 +0600

    Ascending or Descending problem solved

---
+++ src/chapter1/section2/exerecises/AscedingOrDescending.java
@@ -0,0 +1,11 @@
+package chapter1.section2.exerecises;
+
+public class AscedingOrDescending {
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        double z = Double.parseDouble(args[2]);
+
+        System.out.println((x < y && y < z) || (x > y && y > z));
+    }
+}
