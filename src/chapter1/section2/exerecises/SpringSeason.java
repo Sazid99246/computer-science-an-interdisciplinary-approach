73112959e1eda72513d45e04b140b52caf309c13
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-01 09:08:09 +0600

    Spring Season problem solved

---
+++ src/chapter1/section2/exerecises/SpringSeason.java
@@ -0,0 +1,9 @@
+package chapter1.section2.exerecises;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+        System.out.println((m == 3 && d >= 20 && d <= 31) || (m == 4 && d >= 1 && d <= 31) || (m == 5 && d >= 1 && d <= 31) || (m == 6 && d >= 1 && d <= 20));
+    }
+}
