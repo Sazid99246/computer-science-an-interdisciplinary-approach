6686bd127c209d42c98c97136d725919a6baa43b
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-03 07:31:29 +0600

    GreatCircle problm solved

---
+++ src/chapter1/section2/exerecises/GreatCircle.java
@@ -0,0 +1,13 @@
+package chapter1.section2.exerecises;
+
+public class GreatCircle {
+    public static void main(String[] args) {
+        double x1 = Math.toRadians(Double.parseDouble(args[0]));
+        double y1 = Math.toRadians(Double.parseDouble(args[1]));
+        double x2 = Math.toRadians(Double.parseDouble(args[2]));
+        double y2 = Math.toRadians(Double.parseDouble(args[3]));
+        double r = 6371.0;
+
+        System.out.println(2 * r * Math.asin(Math.sqrt(Math.pow(Math.sin((x2 - x1) / 2), 2) + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin((y2 - y1) / 2), 2))) + " kilometers");
+    }
+}
