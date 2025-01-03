c364a1e2b09e2080df1feb1f9b2e595db1cf70c2
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-03 09:45:42 +0600

    EqualOrNot problem solved

--- src/chapter1/section2/webExercises/EqualOrNot.java
+++ src/chapter1/section2/webExercises/EqualOrNot.java
@@ -2,6 +2,10 @@ package chapter1.section2.webExercises;
 
 public class EqualOrNot {
     public static void main(String[] args) {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        int n3 = Integer.parseInt(args[2]);
 
+        System.out.println((n1 == n2) && (n2 == n3));
     }
 }
