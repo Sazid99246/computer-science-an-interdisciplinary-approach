f42ebaa5dfaba7da3bbbde78989a773719707d98
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-05 22:21:37 +0600

    ContinuousCompoundInterest problem solved

--- src/chapter1/section2/exercises/ContinuousCompoundInterest.java
+++ src/chapter1/section2/exercises/ContinuousCompoundInterest.java
@@ -1,14 +1,12 @@
 package chapter1.section2.exercises;
 
-public class LoanPayments {
+public class ContinuousCompoundInterest {
     public static void main(String[] args) {
         int t = Integer.parseInt(args[0]);
         int p = Integer.parseInt(args[1]);
         int r = Integer.parseInt(args[2]);
 
-        double totalAmount = p * Math.exp(r * t);
-        System.out.println(totalAmount / (t * 12));
-
-        System.out.println();
+        double totalAmount = p * Math.exp(((double) r / 100) * t);
+        System.out.println(totalAmount);
     }
 }
