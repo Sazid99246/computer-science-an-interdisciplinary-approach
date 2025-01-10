43239ca47a0c3fa68a97d4af9eb146afb540f6fe
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-10 20:24:39 +0600

    RollDie problem solved

---
+++ src/chapter1/section3/webExercises/RollDie.java
@@ -0,0 +1,8 @@
+package chapter1.section3.webExercises;
+
+public class RollDie {
+    public static void main(String[] args) {
+        int roll = (int) (Math.random() * 6) + 1;
+        System.out.println(roll);
+    }
+}
