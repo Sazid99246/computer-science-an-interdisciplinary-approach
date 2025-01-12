c52143af4932dc0e11c62df2ac06ba0ea5688704
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-12 14:37:57 +0600

    NPerLine solution updated

--- src/chapter1/section3/webExercises/NPerLine.java
+++ src/chapter1/section3/webExercises/NPerLine.java
@@ -5,8 +5,15 @@ public class NPerLine {
         int n = Integer.parseInt(args[0]);
         int count = 0;
 
-        for (int i = 10; i < 100; i++) {
-            System.out.print(i + " ");
+        for (int i = 1; i <= 1000; i++) {
+            System.out.print(i);
+            if (i < 10) {
+                System.out.print("   ");
+            } else if (i < 100) {
+                System.out.print("  ");
+            } else {
+                System.out.print(" ");
+            }
             count++;
             if (count % n == 0) {
                 System.out.println();
